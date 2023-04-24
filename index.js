a43e620739e65728c34d305fee4104483c5d4860
Daniel Menezes <daniel.menezes@infoar.com.br>
2023-04-24 15:54:57 -0300

    ajuste

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const app = express();
-require('dotenv/config')
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
 const port = 8081;
 const user = "danielprofissional93@outlook.com";
 const pass = process.env.USER_PASS_EMAIL;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var corsOptions = {
   origin: "http://localhost:3000",
@@ -17,7 +21,6 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.get("/send/:email/:nome/:mensagem", (req, res) => {
-
   const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
     port: 587,
@@ -31,6 +34,43 @@ app.get("/send/:email/:nome/:mensagem", (req, res) => {
       replyTo: req.params.email,
       subject: `Nova mensagem de ${req.params.nome}.`,
       text: req.params.mensagem,
+    })
+    .then((info) => res.send(info))
+    .catch((erro) => res.send(erro));
+});
+
+app.post("/send-email", (req, res) => {
+  const {
+    nome,
+    email,
+    mensagem,
+    assunto,
+    celular,
+    empresa,
+    funcionarios,
+    posuiLoja,
+  } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    auth: { user, pass },
+  });
+
+  transporter
+    .sendMail({
+      from: user,
+      to: user,
+      replyTo: email,
+      subject: assunto,
+      text: `
+        Nova mansagem de: ${nome}/${email}.
+        Telefone/Celular: ${celular}
+        Empresa: ${empresa}
+        Número de funcionários: ${funcionarios}
+        Já possui empresa virtual? ${posuiLoja}
+        Mensagem: ${mensagem}
+      `,
     })
     .then((info) => res.send(info))
     .catch((erro) => res.send(erro));
